bfe27c01330f8c0fb9ae54a2cd9d96427fc8e178
JaeHyun Lee <myleje@gmail.com>
2021-12-23 15:05:56 +0900

    .

--- Assets/02.Scripts/PlaceMgr.cs
+++ Assets/02.Scripts/PlaceMgr.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class PlaceMgr : MonoBehaviour
 {
     public GameObject mummy;
+
+    private ARRaycastManager raycastManager;
 
     void Start()
     {
