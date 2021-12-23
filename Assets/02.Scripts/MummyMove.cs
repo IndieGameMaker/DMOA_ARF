6f13b267a7e3a9f2a06ee185dca61bc106e28b4a
JaeHyun Lee <myleje@gmail.com>
2021-12-23 14:42:39 +0900

    .

--- Assets/02.Scripts/MummyMove.cs
+++ Assets/02.Scripts/MummyMove.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MummyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //ARCamera의 Transform 컴포넌트를 저장할 변수
+    private Transform camTr;
+    //이동속도
+    public float moveSpeed = 0.5f;
+
     void Start()
     {
-
+        camTr = Camera.main.transform; //Camera.main.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //회전
+        Vector3 lookDir = new Vector3(camTr.position.x, transform.position.y, camTr.position.z);
+        transform.LookAt(lookDir);
+        //이동
+        transform.Translate(transform.forward * Time.deltaTime * moveSpeed);
     }
 }
