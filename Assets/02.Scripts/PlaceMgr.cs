8ad3e127a7539eb76ff2f54f85de5abfc976bc56
JaeHyun Lee <myleje@gmail.com>
2021-12-23 15:18:13 +0900

    .

--- Assets/02.Scripts/PlaceMgr.cs
+++ Assets/02.Scripts/PlaceMgr.cs
@@ -8,16 +8,32 @@ public class PlaceMgr : MonoBehaviour
 {
     public GameObject mummy;
 
+    //ARFoundation의 Raycast를 관리하는 컴포넌트
     private ARRaycastManager raycastManager;
+    //Raycasting 결과를 저장할 리스트
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     void Start()
     {
         mummy = Resources.Load<GameObject>("Mummy");
+        raycastManager = GetComponent<ARRaycastManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //스크린 터치여부를 확인
+        if (Input.touchCount == 0) return;
 
+        //첫번째 손가락 터치 정보를 저장
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            //Raycasting : 2D 좌푯값 -> 3D Raycasting
+            if (raycastManager.Raycast(touch.position, hits, TrackableType.All))
+            {
+                Instantiate(mummy, hits[0].pose.position, hits[0].pose.rotation);
+            }
+        }
     }
 }
